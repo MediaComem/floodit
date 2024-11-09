a957fff5be2e9801fdc87304a59f6a6000439e12
Simon Oulevay <simon.oulevay@gmail.com>
2024-11-09 16:43:42 +0100

    Update to angular 16

--- frontend/src/app/games/continue-current-game.guard.ts
+++ frontend/src/app/games/continue-current-game.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+
 import { GameService } from './game.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ContinueCurrentGameGuard implements CanActivate {
+export class ContinueCurrentGameGuard  {
   constructor(private readonly gameService: GameService) {}
 
   canActivate(): boolean {
